a23102c770e71160967d2d9f02d19bad8dc1403e
Ajinkya Saste <ajinkyamangeshsaste2023@gmail.com>
2025-11-21 21:25:58 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -214,4 +214,5 @@ def reset():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
